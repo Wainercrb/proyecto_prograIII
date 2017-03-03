6404a699de70ee96830e38f9bbb37f8b49f2d5f0
2011.peswainer.1996@gmail.com <61250382>
2017-03-02 21:27:16 -0600

    se crea el menú que servira para en mantenimiento de la aplicacion

--- appTalles/appTalles/vista/Form1.Designer.cs
+++ appTalles/appTalles/vista/Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cambioContraseñaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroEmpleadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parámetrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ediarOrdenesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionGerenciaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sistemaToolStripMenuItem,
+            this.parámetrosToolStripMenuItem,
+            this.ediarOrdenesToolStripMenuItem,
+            this.gestionGerenciaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(630, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sistemaToolStripMenuItem
+            // 
+            this.sistemaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cambioContraseñaToolStripMenuItem,
+            this.registroEmpleadoToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.sistemaToolStripMenuItem.Name = "sistemaToolStripMenuItem";
+            this.sistemaToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.sistemaToolStripMenuItem.Text = "Sistema";
+            // 
+            // cambioContraseñaToolStripMenuItem
+            // 
+            this.cambioContraseñaToolStripMenuItem.Name = "cambioContraseñaToolStripMenuItem";
+            this.cambioContraseñaToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.cambioContraseñaToolStripMenuItem.Text = "Cambio contraseña";
+            // 
+            // registroEmpleadoToolStripMenuItem
+            // 
+            this.registroEmpleadoToolStripMenuItem.Name = "registroEmpleadoToolStripMenuItem";
+            this.registroEmpleadoToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.registroEmpleadoToolStripMenuItem.Text = "Registro empleado";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // parámetrosToolStripMenuItem
+            // 
+            this.parámetrosToolStripMenuItem.Name = "parámetrosToolStripMenuItem";
+            this.parámetrosToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.parámetrosToolStripMenuItem.Text = "Parámetros";
+            // 
+            // ediarOrdenesToolStripMenuItem
+            // 
+            this.ediarOrdenesToolStripMenuItem.Name = "ediarOrdenesToolStripMenuItem";
+            this.ediarOrdenesToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.ediarOrdenesToolStripMenuItem.Text = "Ediar ordenes ";
+            // 
+            // gestionGerenciaToolStripMenuItem
+            // 
+            this.gestionGerenciaToolStripMenuItem.Name = "gestionGerenciaToolStripMenuItem";
+            this.gestionGerenciaToolStripMenuItem.Size = new System.Drawing.Size(107, 20);
+            this.gestionGerenciaToolStripMenuItem.Text = "Gestion gerencia";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 347);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sistemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cambioContraseñaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroEmpleadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parámetrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ediarOrdenesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionGerenciaToolStripMenuItem;
     }
 }
 
